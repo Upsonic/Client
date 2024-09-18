b730bddb818706e62aedd3899aa5b1798a91f0b6
Onur ULUSOY <atadogan06@gmail.com>
2024-09-18 10:04:40 +0300

    chore: Removed unused things

--- upsonic/remote/__init__.py
+++ upsonic/remote/__init__.py
@@ -1,11 +1,6 @@
-from .controller import Upsonic_Remote
+
 from .on_prem import Upsonic_On_Prem, Tiger, Tiger_Admin, UpsonicOnPrem
-from .interface import Upsonic_Cloud_Free
-from .interface import Upsonic_Cloud_Pro
-from .interface import Upsonic_Cloud_Premium
-from .interface import Upsonic_Cloud_Startup
-from .interface import Upsonic_Cloud_Readonly
-from .interface import Upsonic_Cloud_Generic
+
 from .helper import no_exception
 from .helper import requires
 from .interface import encrypt
